9ca2af16ec5b42a6a5de429fd340fa9fd79b4c82
juliasilvae <studyingjulie03@outlook.com>
2025-05-11 23:37:24 -0300

    Add files via upload

---
+++ botao.js
@@ -0,0 +1,15 @@
+document.getElementById("meuBotao").addEventListener("click", function () {
+    const destino = document.getElementById("parte02").offsetTop;
+    window.scrollTo({
+        top: destino,
+        behavior: "smooth"
+    });
+});
+
+/* 
+---------------------------------------------------
+ Criado e Desenvolvido por Renan Dias 
+ Instagram: @zdiazr_
+ LinkedIn: www.linkedin.com/in/renandiasoliveira
+---------------------------------------------------
+*/
